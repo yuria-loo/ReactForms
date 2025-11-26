a308fbcfbfae853466222a13074c39ab069ef491
Yuria Loo <yuria.loo@comcast.net>
2025-11-25 16:42:49 -0800

    Add gitignore and fix some bugs in the import statement

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,12 +1,12 @@
 import RestaurantsContainer from "./components/RestaurantsContainer";
 import AddRestaurant from "./components/addRestaurant";
-import { data } from "./data/data.js";
+import { restaurants } from "../../api/server/data.js";
 import { useState } from "react";
 import "./App.css";
 
 //App is the root of our application and where we load in our components.
 function App() {
-  const [restaurantState, setRestaurants] = useState([...data]);
+  const [restaurantState, setRestaurants] = useState([...restaurants]);
 
   const updateRestaurants = (restaurant) => {
     console.log(restaurant);
