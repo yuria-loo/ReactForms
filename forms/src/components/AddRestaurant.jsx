49a290b7ff339bc08ec79a97e4d16d901d804c57
Yuria Loo <yuria.loo@comcast.net>
2025-11-30 12:15:55 -0800

    Update the add state

--- forms/src/components/AddRestaurant.jsx
+++ forms/src/components/AddRestaurant.jsx
@@ -1,35 +1,55 @@
 import { useState } from "react";
 function AddRestaurant({ updateRestaurants }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    address: "",
+    phone: "",
+    cuisine: "",
+    rating: "",
+    image: "",
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData({...formData, [name]: value});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateRestaurants(formData);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label>Name:</label>
-        <input type="text" name="name" required />
+        <input type="text" name="name" value={formData.name} onChange={handleChange} required />
       </div>
 
       <div>
         <label>image:</label>
-        <input type="text" name="image" required />
+        <input type="text" name="image" value={formData.image} onChange={handleChange} required />
       </div>
 
       <div>
         <label>Address:</label>
-        <input type="text" name="address" required />
+        <input type="text" name="address" value={formData.address} onChange={handleChange} required />
       </div>
 
       <div>
         <label>Phone:</label>
-        <input type="tel" name="phone" required />
+        <input type="tel" name="phone" value={formData.phone} onChange={handleChange} required />
       </div>
 
       <div>
         <label>Cuisine:</label>
-        <input type="text" name="cuisine" required />
+        <input type="text" name="cuisine" value={formData.cuisine} onChange={handleChange} required />
       </div>
 
       <div>
         <label>Rating:</label>
-        <input type="number" name="rating" min="1" max="5" required />
+        <input type="number" name="rating" min="1" max="5" value={formData.rating} onChange={handleChange} required />
       </div>
 
       <button type="submit">Submit</button>
